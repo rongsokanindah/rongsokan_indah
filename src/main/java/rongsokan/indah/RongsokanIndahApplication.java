8f33edbfeeb129068ac67cf6514319cceaded319
rongsokanindah <rongsokan.indah@gmail.com>
2025-04-27 18:15:40 +0700

    first commit

---
+++ src/main/java/rongsokan/indah/RongsokanIndahApplication.java
@@ -0,0 +1,12 @@
+package rongsokan.indah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RongsokanIndahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RongsokanIndahApplication.class, args);
+	}
+}
