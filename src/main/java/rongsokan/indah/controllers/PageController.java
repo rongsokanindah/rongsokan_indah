7692fe346b8436d34ed96a3cc25267305383f88b
rongsokanindah <rongsokan.indah@gmail.com>
2025-05-02 12:52:38 +0700

    add pageController

---
+++ src/main/java/rongsokan/indah/controllers/PageController.java
@@ -0,0 +1,13 @@
+package rongsokan.indah.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String splashPage() {
+        return "pages/splash";
+    }
+}
