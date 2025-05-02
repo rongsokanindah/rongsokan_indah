ff70d1806713ee38d488901d1e0fe5ba38112966
rongsokanindah <rongsokan.indah@gmail.com>
2025-05-02 12:50:59 +0700

    update constant

--- src/main/java/rongsokan/indah/constants/TextConstant.java
+++ src/main/java/rongsokan/indah/constants/TextConstant.java
@@ -2,9 +2,12 @@ package rongsokan.indah.constants;
 
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+
+@Getter
 @Component
 public class TextConstant {
-    public static final String TITLE = "Rongsokan Indah";
-    public static final String DESCRIPTION = "Ini adalah project Sistem Rongsokan Indah";
-    public static final String EMAIL = "rongsakan.indah@gmail.com";
+    private final String title = "Rongsokan Indah";
+    private final String description = "Ini adalah project Sistem Rongsokan Indah";
+    private final String email = "rongsakan.indah@gmail.com";
 }
