51f63be112fba62192b35acc13cc7ff9ef30a192
rongsokanindah <rongsokan.indah@gmail.com>
2025-05-16 13:37:11 +0700

    update controller

--- src/main/java/rongsokan/indah/controllers/PageController.java
+++ src/main/java/rongsokan/indah/controllers/PageController.java
@@ -1,13 +1,26 @@
 package rongsokan.indah.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import rongsokan.indah.services.PageService;
 
 @Controller
 public class PageController {
 
+    @Autowired
+    private PageService pageService;
+
     @GetMapping("/")
     public String splashPage() {
         return "pages/splash";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(HttpServletRequest request, Model model) {
+        return pageService.getLoginPage(request, model);
     }
 }
