8c845e377f7e268c4f9637876ae1d51a0871e127
rongsokanindah <rongsokan.indah@gmail.com>
2025-05-16 13:54:50 +0700

    update repositories

--- src/main/java/rongsokan/indah/repositories/UserRepository.java
+++ src/main/java/rongsokan/indah/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package rongsokan.indah.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ import rongsokan.indah.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    Optional<User> findByUsername(String username);
 }
