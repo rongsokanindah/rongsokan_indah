eb97072275e33fb23d8bc4a42b3488c6a6c565b3
rongsokanindah <rongsokan.indah@gmail.com>
2025-05-16 13:31:13 +0700

    update constant

---
+++ src/main/java/rongsokan/indah/constants/AttributeConstant.java
@@ -0,0 +1,11 @@
+package rongsokan.indah.constants;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+
+@Getter
+@Component
+public class AttributeConstant {
+    private final String loginError = "LOGIN_ERROR";
+}
--- src/main/java/rongsokan/indah/constants/TextConstant.java
+++ src/main/java/rongsokan/indah/constants/TextConstant.java
@@ -10,4 +10,8 @@ public class TextConstant {
     private final String title = "Rongsokan Indah";
     private final String description = "Ini adalah project Sistem Rongsokan Indah";
     private final String email = "rongsakan.indah@gmail.com";
+    private final String masukAkunAnda = "Masuk ke Akun Anda";
+    private final String username = "Username";
+    private final String password = "Password";
+    private final String login = "Login";
 }
