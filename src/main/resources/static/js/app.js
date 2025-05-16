73f9e37439642e3fe3cce6589e8302f453469624
rongsokanindah <rongsokan.indah@gmail.com>
2025-05-16 15:11:09 +0700

    login page

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,11 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-    //Initialize variable
+    //~Initialize variable~//
     const currentUrl = window.location.pathname;
 
-    //Splash to redirect
+    //~Splash Screen~//
     if (currentUrl === "/") {
+      //Splash to redirect
       setTimeout(() => {
         window.location.href = "/login";
-      }, 1000);
+      }, 300);
+    }
+
+    //~Login~//
+    if (currentUrl === "/login") {
+      //Initialize document
+      const login = document.querySelector(".login");
+      const formControl = document.querySelectorAll(".form-control");
+
+      //Ckecking Validation
+      if(login.dataset.error) {
+        login.classList.add("show");
+        formControl.forEach(input => {
+          input.classList.add("is-invalid");
+        });
+      } else {
+        setTimeout(() => {
+          login.classList.add("show");
+        }, 300);
+      }
+
+      //Remove Validation when Input
+      formControl.forEach(input => {
+        input.addEventListener("input", () => {
+          input.classList.remove("is-invalid");
+        });
+      });
     }
 });
+
+function hasQueryParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.has(param);
+}
